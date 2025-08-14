fa75549eab1e0781f5da1a41b51d7a9fddb1cf34
Phenomenon <xiii15499@gmail.com.com>
2025-08-14 10:29:45 +0700

    lecture-11-complete

--- src/components/ModalRegister.tsx
+++ src/components/ModalRegister.tsx
@@ -16,26 +16,44 @@ export default function ModalRegister() {
     setLname(event.target.value);
   };
 
-  const selectPlanOnChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {};
+  const selectPlanOnChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPlan(event.target.value);
+  };
 
-  const radioGenderMaleOnChange = () => {};
+  const radioGenderMaleOnChange = () => {
+    setGender("male");
+  };
 
-  const radioGenderFemaleOnChange = () => {};
+  const radioGenderFemaleOnChange = () => {
+    setGender("female");
+  };
 
-  const cbBuyBottleOnChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {};
+  const cbBuyBottleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyBottle(event.target.checked);
+  };
 
-  const cbBuyShoesOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+  const cbBuyShoesOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyShoes(event.target.checked);
+  };
 
-  const cbBuyCapOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+  const cbBuyCapOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBuyCap(event.target.checked);
+  };
 
-  const computeTotalPayment = () => {};
+  const computeTotalPayment = () => {
+    let total = 0;
+    if (plan === "funrun") total += 500;
+    if (plan === "mini") total += 800;
+    if (plan === "half") total += 1200;
+    if (plan === "full") total += 1500;
+    if (buyBottle) total += 200;
+    if (buyShoes) total += 600;
+    if (buyCap) total += 400;
+    return total;
+  };
 
   const registerBtnOnClick = () => {
-    alert(`Registration complete. Please pay money for ... THB.`);
+    alert(`Registration complete. Please pay money for ${computeTotalPayment().toLocaleString()} THB.`);
   };
 
   return (
@@ -87,8 +105,18 @@ export default function ModalRegister() {
               Mini Marathon 10 Km (800 THB)
               Half Marathon 21 Km (1,200 THB)
               Full Marathon 42.195 Km (1,500 THB) */}
-              <select className="form-select">
+              <select
+                className="form-select"
+                onChange={selectPlanOnChange}
+                value={plan}
+              >
                 <option value="">Please select..</option>
+                <option value="funrun">Fun run 5.5 Km (500 THB)</option>
+                <option value="mini">Mini Marathon 10 Km (800 THB)</option>
+                <option value="half">Half Marathon 21 Km (1,200 THB)</option>
+                <option value="full">
+                  Full Marathon 42.195 Km (1,500 THB)
+                </option>
               </select>
             </div>
 
@@ -96,9 +124,19 @@ export default function ModalRegister() {
             <div>
               <label className="form-label">Gender</label>
               <div>
-                <input className="me-2 form-check-input" type="radio" />
+                <input
+                  className="me-2 form-check-input"
+                  type="radio"
+                  onChange={radioGenderMaleOnChange}
+                  checked={gender === "male"}
+                />
                 Male 👨
-                <input className="mx-2 form-check-input" type="radio" />
+                <input
+                  className="mx-2 form-check-input"
+                  type="radio"
+                  onChange={radioGenderFemaleOnChange}
+                  checked={gender === "female"}
+                />
                 Female 👩
               </div>
             </div>
@@ -107,25 +145,46 @@ export default function ModalRegister() {
             <div>
               <label className="form-label">Extra Item(s)</label>
               <div>
-                <input className="me-2 form-check-input" type="checkbox" />
+                <input 
+                className="me-2 form-check-input" 
+                type="checkbox" 
+                onChange={cbBuyBottleOnChange}
+                checked={buyBottle}
+                />
                 <label className="form-check-label">Bottle 🍼 (200 THB)</label>
               </div>
               <div>
-                <input className="me-2 form-check-input" type="checkbox" />
+                <input 
+                className="me-2 form-check-input" 
+                type="checkbox" 
+                onChange={cbBuyShoesOnChange}
+                checked={buyShoes}
+                />
                 <label className="form-check-label">Shoes 👟 (600 THB)</label>
               </div>
               <div>
-                <input className="me-2 form-check-input" type="checkbox" />
+                <input 
+                className="me-2 form-check-input" 
+                type="checkbox" 
+                onChange={cbBuyCapOnChange}
+                checked={buyCap}
+                />
                 <label className="form-check-label">Cap 🧢 (400 THB)</label>
               </div>
             </div>
 
             {/* Total Payment */}
-            <div>Total Payment : ... THB</div>
+            <div>
+              Total Payment : {computeTotalPayment().toLocaleString()} THB
+            </div>
           </div>
           <div className="modal-footer">
             {/* Register Button */}
-            <button type="button" className="btn btn-success">
+            <button 
+            type="button" 
+            className="btn btn-success"
+            onClick={ registerBtnOnClick }
+            >
               Register
             </button>
           </div>
